0dac820283ed707dc0fb31651d11a16d0617c8c2
Виталий Шалыгин <vtz.2@yandex.ru>
2024-09-17 18:57:44 +0700

    Исправлена ошибка загрузки пользователей

--- Settings.py
+++ Settings.py
@@ -1,9 +1,9 @@
+import json
 import os
 from locale import setlocale, LC_ALL
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
 
 load_dotenv('./workdir/.env')
 
@@ -11,7 +11,7 @@ engine = create_engine('sqlite:///workdir/Myfin.db')
 
 TOKEN = os.getenv('TOKEN')
 
-user_dict = eval(os.getenv('USERS'))
+user_dict = json.load(os.getenv('USERS'))
 # Список пользователей для записи в бд
 
 LANGUAGE = setlocale(LC_ALL, 'ru_RU.UTF-8')
